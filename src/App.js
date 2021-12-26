664ce0a93cb2457e34dce7ac9056811a853f8add
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2021-12-26 15:08:01 +0100

    + add search-box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -8,8 +9,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-        monsters: [
-        ]
+        monsters: [],
+        searchField: ''
       }
   }
 
@@ -20,10 +21,15 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className='App'>
         <h1>Monsters Rolodex</h1>
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
